f0b94c90207486d55e27173d6bdc85a96b6fb53c
Hien Luong <hyhien@gmail.com>
2021-10-16 15:25:26 +0700

    TodoApp POST, PUT, DELETE

--- TodoAppFastApi/app/mainapp.py
+++ TodoAppFastApi/app/mainapp.py
@@ -1,7 +1,8 @@
 import json
 import os
-from fastapi import FastAPI
-from app.models import Product
+import uuid
+from fastapi import FastAPI, HTTPException
+from app.models import Product, TodoModel
 from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
 
@@ -26,10 +27,10 @@ def create_new_product(model: Product.HangHoa):
 
 
 # TODO router
+full_path = os.path.join(Path(__file__).parent, "todos.json")
+
+
 def read_todo_items():
-    folder = Path(__file__).parent
-    # print("Thu muc: ", folder)
-    full_path = os.path.join(folder, "todos.json")
     with open(full_path, "r") as f:
         data = f.read()
         return json.loads(data)
@@ -51,3 +52,53 @@ async def get_todos(id) -> dict:
             break
 
     return {"data": data}
+
+
+@app.post("/todos", tags=["todos"])
+async def add_todo(todo: TodoModel.TodoItem) -> dict:
+    todo.id = str(uuid.uuid4())
+    items = read_todo_items()
+
+    print(type(items), type(items[0]), type(todo))
+    items.append(todo.__dict__)
+
+    print(items)
+    # Save file
+    with open(full_path, "w") as f:
+        json.dump(items, f, indent=4)
+
+    return {
+        "data": todo
+    }
+
+
+@app.put("/todos/{id}", tags=["todos"])
+async def get_todos(id, todo: TodoModel.EditTodoItemModel) -> dict:
+    items = read_todo_items()
+    for item in items:
+        if item["id"] == id:
+            item["name"] = todo.name
+            item["noted"] = todo.noted
+            item["is_completed"] = todo.is_completed
+
+            # Save
+            with open(full_path, "w") as f:
+                json.dump(items, f, indent=4)
+            return {"success": True, "data": todo}
+
+    return HTTPException(status_code=404, detail=f"Item {id} not found")
+
+
+@app.delete("/todos/{id}", tags=["todos"])
+async def remove_todos(id):
+    items = read_todo_items()
+    for item in items:
+        if item["id"] == id:
+            items.remove(item)
+
+            #Save
+            with open(full_path, "w") as f:
+                json.dump(items, f, indent=4)
+            return {"success": True}
+
+    return HTTPException(status_code=404, detail=f"Item {id} not found")
